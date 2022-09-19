69f4fd77622a07d39f8dbbbce7b346a449fcb7bc
YevsInna <evseinka@gmail.com>
2022-09-19 15:08:40 +0200

    lesson 2, user all posts in App.js

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -3,12 +3,12 @@ import React from 'react';
 import '../style.css'
 
 const Post = ({post}) => {
-    const {id, title, body} = post;
+    const {id, userId, title, body} = post;
 
     return (
         <div className="post">
 
-            <h3>Post {post.id}</h3>
+            <h3>User {post.userId} post {post.id}</h3>
             <p>{post.title}</p>
             <p>{post.body}</p>
 

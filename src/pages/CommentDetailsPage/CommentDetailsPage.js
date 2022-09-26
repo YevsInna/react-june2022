92c6b4d8fcb47127f2b9fe22306786b99482de3d
YevsInna <evseinka@gmail.com>
2022-09-26 20:15:37 +0200

    lesson 5 router

--- src/pages/CommentDetailsPage/CommentDetailsPage.js
+++ src/pages/CommentDetailsPage/CommentDetailsPage.js
@@ -1,18 +1,14 @@
 import React, {useEffect, useState} from 'react';
-import {useLocation, useParams} from "react-router";
+import {useParams} from "react-router";
 
 import {postsService} from "../../services";
 import {CommentDetails} from "../../components/CommentDetails/CommentDetails";
 
 const CommentDetailsPage = () => {
-
-    const {state} = useLocation();
     const {postId} = useParams();
     const [post, setPost] = useState(null);
     useEffect(() => {
-        if (!state) {
-            postsService.getById(postId).then(value => setPost(value.data))
-        }
+        postsService.getById(postId).then(value => setPost(value.data))
     }, [postId]);
 
     return (
